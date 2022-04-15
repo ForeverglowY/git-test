3b2bb843aef0596fc6e7f0b05eff34d9bdbac733
zwy <1652199758@qq.com>
2022-04-15 19:07:02 +0800

    first commit

---
+++ src/main/java/com/zwy/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zwy.git;
+
+/**
+ * @author 赵文渊
+ * @version 1.0
+ * @date 2022/4/15 19:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
