a14ced3d25e5c7ee79ba245e410627e3d72160e1
zwy <1652199758@qq.com>
2022-04-15 19:09:29 +0800

    third commit

--- src/main/java/com/zwy/git/GitTest.java
+++ src/main/java/com/zwy/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello bro!");
+        System.out.println("hello man!");
     }
 }
